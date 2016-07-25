83af204dd90f0cb25489848436eec646c42342dd
Joe Karlsson <joe.carlson@callmejoe.net>
2016-07-25 12:22:50 -1000

    huge refactor, but it works.

---
+++ app/entry.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Foo from './Foo.js';
+
+ReactDOM.render(
+  <Foo />,
+  document.getElementById('root')
+);
---
+++ server/webpack/webpack.config.dev.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, '../../app/entry.js'),
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist/'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
+  module: {
+    // preLoaders: [
+    //   {
+    //     test: /(\.js$|\.jsx$)/,
+    //     loader: 'eslint',
+    //     exclude: /node_modules/,
+    //   },
+    // ],
+    loaders: [{
+      test: /(\.js$|\.jsx$)/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+      },
+    }, {
+      test: /\.json?$/,
+      loader: 'json',
+    }, {
+      test: /(\.scss$|\.css$)/,
+      loaders: [
+        'style',
+        'css?modules&importLoaders=1' +
+        '&localIdentName=[path][local]__[hash:base64:5]!sass',
+        'sass',
+      ],
+    }],
+  },
+  eslint: {
+    configFile: '.eslintrc.js',
+    failOnWarning: false,
+    failOnError: false,
+  },
+};
