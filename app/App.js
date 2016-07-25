636d06bd6df47b046c9e4450039a308d225d8134
sgnl <rayrfarias@gmail.com>
2016-07-25 11:39:11 -1000

    adds stub feed data

--- app/App.js
+++ app/App.js
@@ -2,6 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 import Feed from './Feed';
 
+const feedData = [
+  {
+    "id": 1,
+    "body": "I invented the double-click.",
+    "real_name": "Bill Atkinson",
+    "username": "billatkinson1337",
+    "created_at": "2016.07.24 10:27:29"
+  },
+  {
+    "id": 2,
+    "body": "I invented the square burger.",
+    "real_name": "Dave Thomas",
+    "username": "redhaireddevil",
+    "created_at": "2016.07.25 10:27:29"
+  },
+  {
+    "id": 3,
+    "body": "I invented the light bulb.",
+    "real_name": "Thomas Edison",
+    "username": "confEddy1776",
+    "created_at": "2016.07.26 10:27:29"
+  },
+  {
+    "id": 4,
+    "body": "I invented peanut butter",
+    "real_name": "George Washington Carver",
+    "username": "GeeDubCeePB",
+    "created_at": "2016.07.29 10:27:29"
+  },
+  {
+    "id": 5,
+    "body": "I invented self-driving car",
+    "real_name": "Elon Musk",
+    "username": "elonmusk",
+    "created_at": "2016.07.30 10:27:29"
+  }
+];
+
 class App extends Component {
   render() {
     return (
