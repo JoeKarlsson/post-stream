829ab49ba954772446bbe09fbbda4d5374043976
sgnl <rayrfarias@gmail.com>
2016-07-25 10:48:02 -1000

    adds Feed component stub

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Feed from './Feed';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Feed</h1>
+        <h1>Post Stream</h1>
+        <Feed />
       </div>
     );
   }
---
+++ src/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feed extends Component {
+  render() {
+    return (
+        <div className="App">
+          <h1>Feed</h1>
+        </div>
+    );
+  }
+}
+
+export default Feed;
