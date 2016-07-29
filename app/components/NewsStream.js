05a5d09ef8c0279b2b78795a0c746cd3dcb4aa0c
sgnl <rayrfarias@gmail.com>
2016-07-29 10:09:56 -1000

    refactors Post component format

--- app/components/NewsStream.js
+++ app/components/NewsStream.js
@@ -3,7 +3,18 @@ import Post from './Post';
 
 class NewsStream extends Component {
   render() {
-    var posts = this.props.data.map(( post ) => <Post data={ post } key={post.id} />);
+    var posts = this.props.data.map(( post ) => {
+      return (
+        <Post
+          real_name={ post.real_name }
+          username={post.username}
+          created_at={post.created_at}
+          body={post.body}
+          key={post.id}
+          comments={post.hasOwnProperty('comments') ? post.comments : []}
+        />
+      );
+    });
 
     return (
         <div className="App">
