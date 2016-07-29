56880243d1bd57008a7aa0c3a63a7542d09e7b75
sgnl <rayrfarias@gmail.com>
2016-07-29 10:26:03 -1000

    refactors Post component

--- app/components/Post.js
+++ app/components/Post.js
@@ -3,18 +3,27 @@ import React, { Component } from 'react';
 class Post extends Component {
   render() {
     return (
-        <div className="post">
+      <div className="post">
         <header>
-        <span>{this.props.data.real_name}</span>
-        <span>{this.props.data.username}</span>
-        <span>{this.props.data.created_at}</span>
+          <span>{this.props.real_name}</span>
+          <span>{this.props.username}</span>
+          <span>{this.props.created_at}</span>
         </header>
-        <p>{this.props.data.body}</p>
-        
+        <p>{this.props.body}</p>
+        <div className="post-comment-count">
+          <span>
+          {(_ => {
+            let numberOfComments = this.props.comments.length;
+            switch (true) {
+              case numberOfComments > 0: return `[ ${numberOfComments} comments ]`;
+              default: return "No Comments";
+            }
+          })()}
+          </span>
         </div>
+      </div>
     );
   }
 };
-
 
 export default Post;
