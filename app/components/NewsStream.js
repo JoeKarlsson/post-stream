8caf2753475ad45edee94dc75d0afc334d04dd72
sgnl <rayrfarias@gmail.com>
2016-07-29 17:23:58 -1000

    refactors Post - passing props nicer

--- app/components/NewsStream.js
+++ app/components/NewsStream.js
@@ -6,11 +6,8 @@ class NewsStream extends Component {
     var posts = this.props.data.map(( post ) => {
       return (
         <Post
-          real_name={ post.real_name }
-          username={post.username}
-          created_at={post.created_at}
-          body={post.body}
-          key={post.id}
+          {...post}
+        key={post.id}
           comments={post.hasOwnProperty('comments') ? post.comments : []}
         />
       );
