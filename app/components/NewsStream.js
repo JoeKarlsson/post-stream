71ad105fe96078d01252c38739fc99cb965be1de
sgnl <rayrfarias@gmail.com>
2016-07-31 22:08:27 -1000

    refactors code inline

--- app/components/NewsStream.js
+++ app/components/NewsStream.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import Post from './Post';
+import Styles from './NewsStream.scss';
 
 class NewsStream extends Component {
   render() {
-    var posts = this.props.data.map(( post ) => {
-      return (
-        <Post
-          {...post}
-          comments={post.hasOwnProperty('comments') ? post.comments : []}
-          isParentPost={true}
-          key={post.id}
-        />
-      );
-    });
-
     return (
-        <div className="App">
-          <h1>Feed</h1>
-          {posts}
-        </div>
+      <div className={Styles.newsStream}>
+        {this.props.data.map(( post ) => {
+          return (
+              <Post
+            {...post}
+            comments={post.hasOwnProperty('comments') ? post.comments : []}
+            isParentPost={true}
+            key={post.id}
+              />
+          );
+        })}
+      </div>
     );
   }
 }
