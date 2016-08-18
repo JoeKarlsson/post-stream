555ca6e3706a33704c6fc14df3d13ab72a5f8728
Joe Karlsson <joe.carlson@callmejoe.net>
2016-08-17 17:59:49 -1000

    stubbed out redux

--- app/entry.js
+++ app/entry.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from './reducers';
 import NoMatch from './static/noMatch/NoMatch.js';
 import About from './static/about/About.js';
 import NewsStream from './posts/allPosts/NewsStream.js';
 import App from './App';
 
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+const initialState = store.getState();
+console.log('initialState: ', initialState);
+
 ReactDOM.render(
-  <Router history={ browserHistory }>
-    <Route path='/' component={ App }>
-      <IndexRoute component={ NewsStream } />
-      <Route path='/about' component={ About } />
-      <Route path='*' component={ NoMatch } />
-    </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={ browserHistory }>
+      <Route path='/' component={ App }>
+        <IndexRoute component={ NewsStream } />
+        <Route path='/about' component={ About } />
+        <Route path='*' component={ NoMatch } />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ app/reducers/postReducer.js
@@ -0,0 +1,22 @@
+import Immutable from 'immutable';
+
+const initialState = Immutable.List();
+
+const postReducer = (state = initialState, action) => {
+
+  let newState = state;
+
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+      return state.delete(action.index);
+
+    case 'SET_ITEMS':
+      return Immutable.fromJS(action.data);
+
+    default:
+      newState;
+  }
+  return newState;
+};
+
+export default postReducer;
