dc72cee6edc27c5a404296e515f79d4ab993117e
Joe Karlsson <joe.carlson@callmejoe.net>
2016-10-14 20:53:42 -1000

    updated footer stylez

--- app/components/shared/footer/Footer.js
+++ app/components/shared/footer/Footer.js
@@ -3,7 +3,6 @@ import styles from './Footer.scss';
 
 const Footer = ({item}) => (
   <div>
-    <hr/>
     <footer className={styles.footer}>
       <div className={styles.footer_content}>
         <p>/* site by <a href='http://www.rayfarias.com'>ray</a>, <a href='http://jacobyyoung.com/'>jacoby</a> and <a href='http://www.callmejoe.net/'>joe</a> */</p>
