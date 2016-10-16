f6b7a90d6b3d089e63255239082a3dc64232486c
Joe Karlsson <joe.carlson@callmejoe.net>
2016-10-15 15:01:33 -1000

    added headers to trouble shoot mobile CORs issues

--- app/actions/posts/postActions.js
+++ app/actions/posts/postActions.js
@@ -5,10 +5,18 @@ export const POST_SUCCESS = 'POST_SUCCESS';
 export const POST_FAILURE = 'POST_FAILURE';
 
 export const fetchPosts = () => {
+  const data = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'content-type: application/json; charset=utf-8',
+      'access-control-allow-origin': '*',
+    },
+  };
   return {
     [CALL_API]: {
       endpoint: '/post',
       types: [POST_REQUEST, POST_SUCCESS, POST_FAILURE],
+      data,
     }
   }
 };
